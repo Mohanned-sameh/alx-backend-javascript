00dc11ffca26761ff9b3656af248a16a467285e8
mohanned-sameh <mohanned2678@gmail.com>
2024-04-23 21:34:13 +0200

    rewrote getFullBudgetObject

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,9 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  return {
+    ...budget,
+    total: budget.income + budget.gdp + budget.capita,
+  };
+}
