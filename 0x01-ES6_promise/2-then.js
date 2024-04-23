dfe04aa733a0f9d8167f72bb6032ed6cc7697cb2
mohanned-sameh <mohanned2678@gmail.com>
2024-04-23 22:04:07 +0200

    done task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      return new Error();
+    });
+}
