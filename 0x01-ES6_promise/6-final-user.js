b0f36ffe6b91a16cfd24ca72b981086badd569f0
mohanned-sameh <mohanned2678@gmail.com>
2024-04-25 00:54:35 +0200

    Refactor handleProfileSignup function to use promises instead of async/await in 0x01-ES6_promise/6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName
-) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const res = [];
-  const user = await signUpUser(firstName, lastName).catch(() => null);
-  const photo = await uploadPhoto(fileName).catch(() => null);
 
-  if (user === null || photo === null) {
-    res.push({ status: 'rejected', value: user });
-  } else {
-    res.push({ status: 'fulfilled', value: user });
-  }
-  return res;
+  return signUpUser(firstName, lastName)
+    .then((data) => {
+      res.push({ status: 'fulfilled', value: data });
+    })
+    .catch((err) => {
+      res.push({ status: 'rejected', value: err.toString() });
+    });
 }
