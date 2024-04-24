6bca3d7a224bfc1411e889199974c389953e41a1
mohanned-sameh <mohanned2678@gmail.com>
2024-04-25 00:49:14 +0200

    Refactor handleProfileSignup function to use async/await and separate try-catch blocks in 0x01-ES6_promise/6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,20 +1,36 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return new Promise((resolve, reject) => {
-    signUpUser(firstName, lastName)
-      .then((result) => {
-        uploadPhoto(fileName)
-          .then((photo) => {
-            resolve({ status: 'fulfilled', value: result, photo });
-          })
-          .catch((error) => {
-            resolve({ status: 'rejected', value: error, photo: null });
-          });
-      })
-      .catch((error) => {
-        resolve({ status: 'rejected', value: error, photo: null });
-      });
-  });
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName
+) {
+  const res = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    res.push({
+      status: 'fulfilled',
+      value: user,
+    });
+  } catch (error) {
+    res.push({
+      status: 'rejected',
+      value: error.toString(),
+    });
+  }
+
+  try {
+    const uploader = await uploadPhoto(fileName);
+    res.push({
+      status: 'fulfilled',
+      value: uploader,
+    });
+  } catch (error) {
+    res.push({
+      status: 'rejected',
+      value: error.toString(),
+    });
+  }
+  return res;
 }
