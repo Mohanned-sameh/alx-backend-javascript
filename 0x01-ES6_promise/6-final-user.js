e1c4e61805173f9bef694e1ea93714e2b2038902
mohanned-sameh <mohanned2678@gmail.com>
2024-04-25 00:55:30 +0200

    Refactor handleProfileSignup function to use promises for asynchronous operations in 0x01-ES6_promise/6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,12 +3,26 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const res = [];
-
-  return signUpUser(firstName, lastName)
-    .then((data) => {
-      res.push({ status: 'fulfilled', value: data });
-    })
-    .catch((err) => {
-      res.push({ status: 'rejected', value: err.toString() });
-    });
+  new Promise((resolve, reject) => {
+    res.push(
+      signUpUser(firstName, lastName)
+        .then((data) => {
+          res.push(data);
+        })
+        .catch((err) => {
+          res.push({ status: 'rejected', value: err.toString() });
+        })
+    );
+    res.push(
+      uploadPhoto(fileName)
+        .then((data) => {
+          res.push(data);
+        })
+        .catch((err) => {
+          res.push({ status: 'rejected', value: err.toString() });
+        })
+    );
+    resolve(res);
+  });
+  return res;
 }
