cd8b43fc4e125e39bd1e2963045b2ccdb86ce2d0
mohanned-sameh <mohanned2678@gmail.com>
2024-04-25 19:03:53 +0200

    Refactor import statement and initializeRooms function in 0x02-ES6_classes/1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,7 @@
-import ClassRoom from './0-classroom';
+import Classroom from './0-classroom';
 
 function initializeRooms() {
-  return [new ClassRoom(10), new ClassRoom(20), new ClassRoom(30)];
+  return [new Classroom(19), new Classroom(20), new Classroom(34)];
 }
+
+export default initializeRooms;
