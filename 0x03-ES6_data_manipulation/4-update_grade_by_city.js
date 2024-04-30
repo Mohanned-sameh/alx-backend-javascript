d37967301a6b57f068b8f6f37297bc3455040075
mohanned-sameh <mohanned2678@gmail.com>
2024-04-30 15:43:14 +0300

    Update student grade by city in 0x03-ES6_data_manipulation/4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -9,9 +9,7 @@ export default function updateStudentGradeByCity(
       const grade = newGrades.filter(
         (newGrade) => newGrade.studentId === student.id
       );
-      if (grade.length === 0) {
-        grade.push({ studentId: student.id });
-      }
-      return { ...student, grade: grade[0].grade };
+      student.grade = grade.length > 0 ? grade[0].grade : 'N/A';
+      return student;
     });
 }
