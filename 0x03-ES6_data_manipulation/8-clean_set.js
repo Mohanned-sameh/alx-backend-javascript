cf71e74b228e6f722dfd64958ed8326db2f19bd3
mohanned-sameh <mohanned2678@gmail.com>
2024-04-30 16:00:33 +0300

    Refactor cleanSet function to use forEach loop instead of filter and fix startString check in 0x03-ES6_data_manipulation/8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,20 @@
 export default function cleanSet(set, startString) {
-  const newSet = new Set();
-  set.forEach((value) => {
-    if (typeof value === 'string' && value.startsWith(startString)) {
-      newSet.add(value);
+  let str = '';
+
+  if (startString === '') {
+    return '';
+  }
+  for (const item of set) {
+    if (set instanceof Set && typeof startString === 'string') {
+      if (item && item.startsWith(startString)) {
+        const strSlice = item.slice(startString.length);
+        if (str !== '') {
+          str += '-';
+        }
+        str += strSlice;
+      }
     }
-  });
-  return newSet;
+  }
+
+  return str;
 }
