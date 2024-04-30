dad47868ce104646b5fbbb1453d36ce3b6530485
mohanned-sameh <mohanned2678@gmail.com>
2024-04-30 16:18:48 +0300

    Refactor queryAPI function to use count variable and improve error handling

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,16 +1,12 @@
 const weakMap = new WeakMap();
 let count = 0;
+
 function queryAPI(endpoint) {
-  if (weakMap.get(endpoint) >= 5) {
-    throw Error('Endpoint load is high');
+  weakMap.set(endpoint, (count += 1));
+  const callsNum = weakMap.get(endpoint);
+  if (callsNum >= 5) {
+    throw new Error('Endpoint load is high.');
   }
-  if (weakMap.get(endpoint)) {
-    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  } else {
-    weakMap.set(endpoint, 1);
-  }
-  count += 1;
-  return weakMap;
 }
 
 export { weakMap, queryAPI };
