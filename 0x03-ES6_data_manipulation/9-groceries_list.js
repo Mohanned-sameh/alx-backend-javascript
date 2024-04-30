3d9c547289651a8364cb4952ef26c9a6b84e7262
mohanned-sameh <mohanned2678@gmail.com>
2024-04-30 15:58:46 +0300

    done task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,8 @@
+export default function groceriesList() {
+  const groceries = new Map();
+  groceries.set('Banana', 4);
+  groceries.set('Apple', 6);
+  groceries.set('Pineapple', 3);
+  groceries.set('Orange', 5);
+  return groceries;
+}
